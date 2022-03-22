f39cd4b768c9de26fcce0ed0ee5191160d6419f4
Juan Pablo Castiglione <juanpablocastiglione01@gmail.com>
2022-03-21 23:22:17 -0300

    changed structure

---
+++ script.js
@@ -0,0 +1,15 @@
+let time = '';
+setInterval(async function (){
+    notif = await fetch('https://meli-notif-back.herokuapp.com/notifications/')
+    .then(e => e.json());
+
+    if(time.length != '' & notif.sent != time) {
+        console.log('hola')
+        let audio = new Audio('../static/that-was-quick-606.mp3');
+        audio.play();
+    };
+    console.log(time)
+    console.log(notif.sent)
+    console.log(notif.sent != time);
+    time = notif.sent.slice();
+}, 1000);
